39310b29076b5ac285e249fd03666b706a2e10a4
Rin <ry00001@protonmail.com>
2022-04-25 10:13:58 +0100

    count

--- l_plus_ratio.sh
+++ l_plus_ratio.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-echo "Ell plus ratio plus $(aspell dump master | shuf | head -n 15 | tee /dev/stderr | sed -z "s/'s//g;s/[[:space:]]*$//g;s/\n/ plus /g")" | cargo r | tee output.mp3 | ffplay -
+
+COUNT=10
+if [ -n "$1" ]; then
+    COUNT=$1
+fi
+
+echo "Ell plus ratio plus $(aspell dump master | shuf | head -n $COUNT | tee /dev/stderr | sed -z "s/'s//g;s/[[:space:]]*$//g;s/\n/ plus /g")" | cargo r | tee output.mp3 | ffplay -
